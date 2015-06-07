b0dbea0d08ef7da516d3a058029703a7850d880f
harshal-joshi <harshal.github@gmail.com>
2015-06-07 13:34:04 +0530

    Added new method for new customer

--- src/main/java/com/harshal/spring/restpractice/classicmodels/CustomerController.java
+++ src/main/java/com/harshal/spring/restpractice/classicmodels/CustomerController.java
@@ -3,11 +3,11 @@ package com.harshal.spring.restpractice.classicmodels;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -45,10 +45,22 @@ public class CustomerController {
 	}
 	
 	@RequestMapping(value="{customerId}",produces={MediaType.APPLICATION_JSON_VALUE},method=RequestMethod.GET)
-	public ResponseEntity<Resource<Customer>> customers(@PathVariable int customerId){
+	public ResponseEntity<Customer> customer(@PathVariable int customerId){
 		
-		System.out.println("In get Customer By Id");
-		Customer customer = customerDao.getCustomerById(customerId);
-		return new ResponseEntity(customer,HttpStatus.OK);
+		System.out.println("In get Customer By Id: "+customerId);
+		return new ResponseEntity<>(customerDao.getCustomerById(customerId),HttpStatus.OK);
+		/*return Optional.of(customerDao.getCustomerById(customerId))
+		.map(u -> new ResponseEntity<Customer>(u, HttpStatus.OK))
+		.orElse(new ResponseEntity<Customer>(HttpStatus.NOT_FOUND));*/
+	}
+	
+	@RequestMapping(method=RequestMethod.POST, consumes={MediaType.APPLICATION_JSON_VALUE}, produces={MediaType.APPLICATION_JSON_VALUE})
+	public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer){
+		
+		System.out.println("In register customer profile");
+		return new ResponseEntity<>(customerDao.createCustomer(customer),HttpStatus.OK);
+		/*return Optional.of(customerDao.createCustomer(customer))
+				.map(u -> new ResponseEntity<Customer>(u, HttpStatus.OK))
+				.orElse(new ResponseEntity<Customer>(HttpStatus.INTERNAL_SERVER_ERROR));*/
 	}
 }
