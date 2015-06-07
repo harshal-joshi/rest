cd112ff744d132dd740ce5d46aba336d49e92eec
harshal-joshi <harshal.github@gmail.com>
2015-06-07 13:37:23 +0530

    Dao Change for add, get Customer details

--- src/main/java/com/harshal/spring/restpractice/classicmodels/dao/CustomerDaoImpl.java
+++ src/main/java/com/harshal/spring/restpractice/classicmodels/dao/CustomerDaoImpl.java
@@ -10,11 +10,15 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 import com.harshal.spring.restpractice.classicmodels.CustomerController;
+import com.harshal.spring.restpractice.classicmodels.exceptions.CustomerNotFoundException;
 import com.harshal.spring.restpractice.classicmodels.pojos.Customer;
 import com.harshal.spring.restpractice.classicmodels.pojos.CustomerResponse;
 
@@ -86,15 +90,78 @@ public class CustomerDaoImpl implements CustomerDao{
 
 	public Customer getCustomerById(int customerId) {
 			Map<String,Object> paramMap = new HashMap<String, Object>();
-			paramMap.put("customerid", customerId);
-			Customer customer = parametrisedJdbcTemplate.queryForObject("SELECT * FROM customers WHERE customerid=:customerId", paramMap, Customer.class);
+			paramMap.put("customerId", customerId);
+			Customer customer =null;
+			try {
+				customer = parametrisedJdbcTemplate.queryForObject("SELECT * FROM customers WHERE customerNumber=:customerId", paramMap, new RowMapper<Customer>() {
+
+					@Override
+					public Customer mapRow(ResultSet resultSet, int arg1)
+							throws SQLException {
+						Customer customer = new Customer();
+					 	customer.setCustomerId(resultSet.getInt("customerNumber"));
+						customer.setCustomerName(resultSet.getString("customerName"));
+						customer.setContactFirstName(resultSet.getString("contactFirstName"));
+						customer.setContactLastName(resultSet.getString("contactLastName"));
+						customer.setPhone(resultSet.getString("phone"));
+						customer.setAddressLine(resultSet.getString("addressLine1"));
+						customer.setAddressLine2(resultSet.getString("addressLine2"));
+						customer.setCity(resultSet.getString("city"));
+						customer.setState(resultSet.getString("state"));
+						customer.setPostalCode(resultSet.getString("postalCode"));
+						customer.setCountry(resultSet.getString("country"));
+						customer.setSalesRepEmployeeNumber(resultSet.getInt("salesRepEmployeeNumber"));
+						customer.setCreditLimit(resultSet.getFloat("creditLimit"));
+						return customer;
+					}
+					
+				});
+			}catch(EmptyResultDataAccessException ex) {
+				throw new CustomerNotFoundException(customerId);
+			}catch (DataAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
 			customer.add(linkTo(CustomerController.class).slash(customerId).withSelfRel());
 			return customer;
 	}
 
 	public Customer createCustomer(Customer customer) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		String sql = "INSERT INTO customers(customerNumber, customerName, contactLastName, contactFirstName, phone, addressLine1, "
+				+ "addressLine2, city, state, postalCode, country, salesRepEmployeeNumber, creditLimit) VALUES ("
+				+ ":customerNumber, :customerName, :contactLastName, :contactFirstName, :phone, :addressLine1, "
+				+ ":addressLine2, :city, :state, :postalCode, :country, :salesRepEmployeeNumber, :creditLimit)";
+		
+		Map<String,Object> paramMap = new HashMap<String,Object>();
+		paramMap.put("customerNumber", customer.getCustomerId());
+		paramMap.put("customerName", customer.getCustomerName());
+		paramMap.put("contactLastName", customer.getContactLastName());
+		paramMap.put("contactFirstName", customer.getContactFirstName());
+		paramMap.put("phone", customer.getPhone());
+		paramMap.put("addressLine1", customer.getAddressLine());
+		paramMap.put("addressLine2", customer.getAddressLine2());
+		paramMap.put("city", customer.getCity());
+		paramMap.put("state", customer.getState());
+		paramMap.put("postalCode", customer.getPostalCode());
+		paramMap.put("country", customer.getCountry());
+		paramMap.put("salesRepEmployeeNumber", customer.getSalesRepEmployeeNumber());
+		paramMap.put("creditLimit", customer.getCreditLimit());
+		
+		int count = parametrisedJdbcTemplate.update(sql, paramMap);
+		
+		System.out.println("count ::"+count);
+		
+		if(count>0)
+		{
+			customer.add(linkTo(CustomerController.class).slash(customer.getCustomerId()).withSelfRel());
+			return customer;
+		}
+		else
+		{
+			return null;
+		}
 	}
 
 	public Customer updateCustomerDetails(Customer customer) {
